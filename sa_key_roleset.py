c51115fc672c48614dfb4e83bbaab8f992429ba1
jjfr38760 <62535901+jjfr38760@users.noreply.github.com>
2021-04-22 18:47:35 +0200

    Update sa_key_roleset.py

--- sa_key_roleset.py
+++ sa_key_roleset.py
@@ -7,10 +7,10 @@ def login_auth_github(vault_addr, vault_ca, github_pat):
      return vault_token
 
 
-def get-sa-key(vault_addr, vault_ca, github_pat):	
+def get_sa_key(vault_addr, vault_ca, github_pat):	
      vault_token = login_auth_github(vault_addr, vault_ca, github_pat)
      sa_key = vault_token + "good job"
      return sa_key
 
 if __name__ == '__main__':
-    return get-sa-key(*sys.argv[1:])
+    return get_sa_key(*sys.argv[1:])
