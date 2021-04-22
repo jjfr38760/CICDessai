1a8be1a96ce3c648a6f053e0fc275c5d647ca35a
jjfr38760 <62535901+jjfr38760@users.noreply.github.com>
2021-04-22 18:40:46 +0200

    Update sa_key_roleset.py

--- sa_key_roleset.py
+++ sa_key_roleset.py
@@ -3,25 +3,13 @@ import sys
 
 
 def login_auth_github(vault_addr, vault_ca, github_pat):
-     addr = vault_addr + '/v1/auth/github/FRA-CES-IAC/login'
-     response = requests.post(
-           addr, verify=vault_ca,
-           data=json.dumps({"token": github_pat})
-     )
-     response_body = response.json()
-     vault_token = response_body["auth"]["client_token"]
+     vault_token = vault_addr + vault_ca + github_pat
      return vault_token
 
 
 def get-sa-key(vault_addr, vault_ca, github_pat):	
      vault_token = login_auth_github(vault_addr, vault_ca, github_pat)
-     addr = vault_addr + '/v1/gcp/key/my-key-roleset'
-     response = requests.get(
-           addr, verify=vault_ca,
-           headers={'X-Vault-Token': vault_token},
-     )
-     response_body = response.json()
-     sa_key = response_body["data"]["private_key_data"]
+     sa_key = vault_token + "good job"
      return sa_key
 
 if __name__ == '__main__':
